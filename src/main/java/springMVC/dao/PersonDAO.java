b1c7044986f532d1c81e5cd39ccb9f3b26436e50
Solomakhin Anton <s.anton45@yandex.ru>
2021-11-04 20:49:42 +0300

    CRUD

---
+++ src/main/java/springMVC/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package springMVC.dao;
+
+import org.springframework.stereotype.Component;
+import springMVC.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT,  "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id){
+        // Лямбда - выражение: среди people ищем человека с id == id на входе, если нет - возвращаем null
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
