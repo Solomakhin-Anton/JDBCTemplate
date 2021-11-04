da112dfbbdb1ae2234b88573e5e131027f65889b
Solomakhin Anton <s.anton45@yandex.ru>
2021-11-04 15:05:56 +0300

    requestParam

--- src/main/java/springMVC/controllers/FirstController.java
+++ src/main/java/springMVC/controllers/FirstController.java
@@ -3,18 +3,45 @@ package springMVC.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
+    /*
+    Первый вариант - HttpServletRequest
+     */
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//
+//        return "first/hello";
+//    }
+
+    /*
+    Второй вариант - @RequestParam
+     */
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage() {
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/goodbye";
     }
 }
